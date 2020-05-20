98bdb25ab51a7471ba6a718908c293a9388d758d
Aniket Mahajan <007aniketmahajan@gmail.com>
2020-05-20 21:21:13 +0530

    Add files via upload

---
+++ build/precache-manifest.3b6514a679ae2a1996e85795d76ddee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae53c917bd27dd0db903dfeab7e45ba",
+    "url": "/shareinterest/build/index.html"
+  },
+  {
+    "revision": "dd6982294092c4a7d8c8",
+    "url": "/shareinterest/build/static/css/main.89e58962.chunk.css"
+  },
+  {
+    "revision": "07675f3d571ef88f923b",
+    "url": "/shareinterest/build/static/js/2.26458cb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shareinterest/build/static/js/2.26458cb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd6982294092c4a7d8c8",
+    "url": "/shareinterest/build/static/js/main.c5459f2c.chunk.js"
+  },
+  {
+    "revision": "d5fe6ffede414e43a511",
+    "url": "/shareinterest/build/static/js/runtime-main.b5441832.js"
+  }
+]);
